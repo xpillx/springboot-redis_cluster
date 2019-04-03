6efe30683f2b9b1894cfb1a062d54a826825d0bb
guowei <guowei@joymeng.com>
2019-04-03 19:09:08 +0800

    test

---
+++ src/main/java/com/pill/demo/App.java
@@ -0,0 +1,16 @@
+package com.pill.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口处
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+}
