bd9d6dd6b82beebabb01c2a2478325a8a9a7f8d9
guowei <guowei@joymeng.com>
2019-04-03 19:22:56 +0800

    test

---
+++ src/main/java/com/pill/demo/bean/User.java
@@ -0,0 +1,46 @@
+package com.pill.demo.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author guowei
+ * @version 1.0
+ * @description 测试
+ * @date 2019/4/2 14:17
+ */
+public class User implements Serializable{
+    private String name;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
